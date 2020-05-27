12b304879ad73d8eb19952d1e30fd9920258f538
Shawn Lee <shawnlsj97@gmail.com>
2020-05-27 13:53:29 +0800

    Add firebase setup

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function ProjectSummary() {
+function ProjectSummary({project}) {
     return (
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
+                <span className="card-title">{project.title}</span>
                     <p>Posted by Shawn</p>
                     <p className="grey-text">9th June 2020, 2am</p>
             </div>
---
+++ src/config/fbConfig.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC-GRaweG7dCAfbC41lDBBLVlIAtjDkfyQ",
+    authDomain: "marioplan-9ff69.firebaseapp.com",
+    databaseURL: "https://marioplan-9ff69.firebaseio.com",
+    projectId: "marioplan-9ff69",
+    storageBucket: "marioplan-9ff69.appspot.com",
+    messagingSenderId: "637767691847",
+    appId: "1:637767691847:web:e6c28262bd599a3d180b62",
+    measurementId: "G-CERN59JMQ0"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebase;
