2457185428f3cb73bdba6a48f41db782c87a1c05
Shawn Lee <shawnlsj97@gmail.com>
2020-05-27 14:38:52 +0800

    Add create project feature

--- src/index.js
+++ src/index.js
@@ -3,20 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { createFirestoreInstance, getFirestore, reduxFirestore } from 'redux-firestore'
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase'
+import fbConfig from './config/fbConfig'
+import firebase from 'firebase/app'
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+// compose to use multiple store enhancers (similar to how we combine multiple reducers in root reducer)
+const store = createStore(rootReducer, 
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reduxFirestore(firebase, fbConfig)
+  )
+);
+
+const rrfProps = {
+  firebase,
+  config: fbConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+          <App />
+      </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,18 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
-        // make async call to database
-        dispatch({ type:'CREATE_PROJECT', project});
+    // we can return a function since we are using thunk (halts dispatch)
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
+        // make async call to database then dispatch action after
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Shawn',
+            authorLastName: 'Lee',
+            authorId: 12345,
+            creteAt: new Date()
+        }).then(() => {
+            dispatch({ type:'CREATE_PROJECT', project});
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err })
+        })
     }
 };
